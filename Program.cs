2f6d23d48a1782c001f55e1102a6c896e5f03050
Michaela Murray <10012591@bcs.net.nz>
2017-08-07 14:06:15 +1200

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week5_tutorial1_comp5007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
