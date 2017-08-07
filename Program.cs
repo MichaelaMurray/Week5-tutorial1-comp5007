b5ba4a69aa4ced3afa8ec1b69150cc4944e3a4a0
Michaela Murray <10012591@BCS.NET.NZ>
2017-08-07 14:50:00 +1200

    comi1

--- Program.cs
+++ Program.cs
@@ -4,25 +4,34 @@ namespace week5_tutorial1_comp5007
 {
     public class Shape
     {
+        public void Draw() 
+        {}
+    
+        public class Rectangle : Shape
+        {
 
-    }
-    public class Rectangle : Shape
-    {
+        }
 
-    }
+        public class Circle : Shape
+        {
 
-    public class Circle : Shape
-    {
+        }
 
-    }
+        public class Triangle : Shape
+        {
 
-    public class Triangle : Shape
-    {
-
-    }    
+        }    
         static void Main(string[] args)
         {
-            
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Shape(Triangle));
+            shapes.Add(new Shape(Circle));
+            shapes.Add(new Shape(Rectangle));
+
+            foreach (Shape shapes in shapes)
+            {
+              Console.Draw()  
+            }
         }
     }
 }
